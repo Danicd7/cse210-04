2266ec48831ceb8cd9462ac9ce87ce7ecbb290b0
ProgrammerGLQ <qui22003@byui.edu>
2022-06-09 11:34:06 -0300

    update

---
+++ player.py
@@ -0,0 +1,8 @@
+from actor import Actor
+
+
+class Player(Actor):
+    def __init__(self):
+        super().__init__()
+        self._name = 'player'
+
---
+++ screen.py
@@ -0,0 +1,10 @@
+class Screen:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def getY(self):
+        return self._y
+
+    def getX(self):
+        return self._x
